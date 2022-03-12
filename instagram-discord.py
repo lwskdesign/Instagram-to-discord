3adeea5f3c9af215db05141c04d2ca8061f85998
Tim <40317256+lwskdesign@users.noreply.github.com>
2022-03-12 21:41:53 +0100

    own changes

--- instagram-discord.py
+++ instagram-discord.py
@@ -1,115 +1,86 @@
-#!/usr/bin/python
+#!/usr/bin/env python3
+# SOURCE: https://github.com/fernandod1/Instagram-to-discord/blob/master/instagram-discord.py
 
-# Copyright (c) 2020 Fernando
-# Url: https://github.com/fernandod1/
-# License: MIT
-
-# DESCRIPTION:
-# This script executes 2 actions:
-# 1.) Monitors for new image posted in a instagram account.
-# 2.) If found new image, a bot posts new instagram image in a discord channel.
-# 3.) Repeat after set interval.
-
-# REQUIREMENTS:
-# - Python v3
-# - Python module re, json, requests
-import re
 import json
-import sys
 import requests
-import urllib.request
 import os
 import time
+import dotenv
 
-# USAGE:
-# Set Environment Variables:
-# Set IG_USERNAME to username account you want to monitor. Example - ladygaga
-# Set WEBHOOK_URL to Discord account webhook url. To know how, just Google: "how to create webhook discord".
-# Set TIME_INTERVAL to the time in seconds in between each check for a new post. Example - 1.5, 600 (default=600)
-# Help: https://www.serverlab.ca/tutorials/linux/administration-linux/how-to-set-environment-variables-in-linux/
+dotenv_file = dotenv.find_dotenv()
+dotenv.load_dotenv(dotenv_file)
 
-INSTAGRAM_USERNAME = os.environ.get('IG_USERNAME')
-
-# ----------------------- Do not modify under this line ----------------------- #
-
+INSTAGRAM_USERNAME = os.getenv("INSTAGRAM_USERNAME")
+WEBHOOK_URL = os.getenv("WEBHOOK_URL")
+TIME_INTERVAL = os.getenv("TIME_INTERVAL")
+LAST_IMAGE_ID = os.getenv("LAST_IMAGE_ID")
 
 def get_user_fullname(html):
     return html.json()["graphql"]["user"]["full_name"]
 
-
 def get_total_photos(html):
     return int(html.json()["graphql"]["user"]["edge_owner_to_timeline_media"]["count"])
-
 
 def get_last_publication_url(html):
     return html.json()["graphql"]["user"]["edge_owner_to_timeline_media"]["edges"][0]["node"]["shortcode"]
 
-
 def get_last_photo_url(html):
     return html.json()["graphql"]["user"]["edge_owner_to_timeline_media"]["edges"][0]["node"]["display_url"]
-
 
 def get_last_thumb_url(html):
     return html.json()["graphql"]["user"]["edge_owner_to_timeline_media"]["edges"][0]["node"]["thumbnail_src"]
 
-
 def get_description_photo(html):
     return html.json()["graphql"]["user"]["edge_owner_to_timeline_media"]["edges"][0]["node"]["edge_media_to_caption"]["edges"][0]["node"]["text"]
 
-
 def webhook(webhook_url, html):
-    # for all params, see https://discordapp.com/developers/docs/resources/webhook#execute-webhook
-    # for all params, see https://discordapp.com/developers/docs/resources/channel#embed-object
+    """
+    for all params, see https://discordapp.com/developers/docs/resources/webhook#execute-webhook and https://discordapp.com/developers/docs/resources/channel#embed-object
+    """
     data = {}
     data["embeds"] = []
     embed = {}
     embed["color"] = 15467852
-    embed["title"] = "New pic of @"+INSTAGRAM_USERNAME+""
+    embed["title"] = f"@{INSTAGRAM_USERNAME} posted a new image"
     embed["url"] = "https://www.instagram.com/p/" + \
         get_last_publication_url(html)+"/"
     embed["description"] = get_description_photo(html)
-    # embed["image"] = {"url":get_last_thumb_url(html)} # unmark to post bigger image
-    embed["thumbnail"] = {"url": get_last_thumb_url(html)}
+    embed["image"] = {"url":get_last_thumb_url(html)}
     data["embeds"].append(embed)
-    result = requests.post(webhook_url, data=json.dumps(
-        data), headers={"Content-Type": "application/json"})
+    result = requests.post(webhook_url, data=json.dumps(data), headers={"Content-Type": "application/json"})
     try:
         result.raise_for_status()
     except requests.exceptions.HTTPError as err:
         print(err)
     else:
-        print("Image successfully posted in Discord, code {}.".format(
-            result.status_code))
-
+        print("Image successfully posted, code {}.".format(result.status_code))
 
 def get_instagram_html(INSTAGRAM_USERNAME):
     headers = {
         "Host": "www.instagram.com",
         "User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11"
     }
-    html = requests.get("https://www.instagram.com/" +
-                        INSTAGRAM_USERNAME + "/feed/?__a=1", headers=headers)
+    html = requests.get("https://www.instagram.com/" + INSTAGRAM_USERNAME + "/feed/?__a=1", headers=headers)
     return html
 
-
 def main():
+    global last_image_id
     try:
         html = get_instagram_html(INSTAGRAM_USERNAME)
-        if(os.environ.get("LAST_IMAGE_ID") == get_last_publication_url(html)):
-            print("Not new image to post in discord.")
+        if(LAST_IMAGE_ID == get_last_publication_url(html)):
+            print("No new image found.")
         else:
-            os.environ["LAST_IMAGE_ID"] = get_last_publication_url(html)
-            print("New image to post in discord.")
-            webhook(os.environ.get("WEBHOOK_URL"),
+            dotenv.set_key(dotenv_file, "LAST_IMAGE_ID", get_last_publication_url(html))
+            print("Found new image.")
+            webhook(WEBHOOK_URL,
                     get_instagram_html(INSTAGRAM_USERNAME))
     except Exception as e:
         print(e)
 
-
 if __name__ == "__main__":
-    if os.environ.get('IG_USERNAME') != None and os.environ.get('WEBHOOK_URL') != None:
+    if INSTAGRAM_USERNAME != None and WEBHOOK_URL != None:
         while True:
             main()
-            time.sleep(float(os.environ.get('TIME_INTERVAL') or 600)) # 600 = 10 minutes
+            time.sleep(float(TIME_INTERVAL or 600))
     else:
-        print('Please configure environment variables properly!')
+        print("Faulty environment variables!")
